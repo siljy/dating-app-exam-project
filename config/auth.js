225b0cdbceb9fdf79e8edfac5491e7b9849433eb
siljy <siljeskogstrand@gmail.com>
2025-05-15 12:20:58 +0200

    Final code cleanup

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,7 @@
 export const url = "https://crudcrud.com/api";
-export const urlKey = "81bf2576047c41b8bae9f3525be3c3aa";
+export const urlKey = "dcfc219a03bc4294a9a7c9f68598d77e";
 
 export const userUrl = `${url}/${urlKey}/userdatabase`;
 export const favUrl = `${url}/${urlKey}/favorites`;
 
-// const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 export const randomUserUrl = "https://randomuser.me/api";
